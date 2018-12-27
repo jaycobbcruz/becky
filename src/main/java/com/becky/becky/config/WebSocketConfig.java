78bcfb8a55faba57a00599dab8745dc4c286dc41
Jaycobb Cruz <jaycobbcruz@gmail.com>
2018-12-27 22:55:57 +0800

    cross-domain ws

--- src/main/java/com/becky/becky/config/WebSocketConfig.java
+++ src/main/java/com/becky/becky/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(messageHandler(), "/message").withSockJS();
-        registry.addHandler(messageHandler(), "/ws");
+        registry.addHandler(messageHandler(), "/ws").setAllowedOrigins("*");
     }
 
     @Bean
